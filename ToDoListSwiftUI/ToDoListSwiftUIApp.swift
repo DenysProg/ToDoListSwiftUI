f0402b8609945af187ec9afe935c14428eb4eee3
DenysProg <denysnikolaichuk@gmail.com>
2023-03-07 09:15:52 +0200

    Initial Commit

---
+++ ToDoListSwiftUI/ToDoListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListSwiftUIApp.swift
+//  ToDoListSwiftUI
+//
+//  Created by Denys Nikolaichuk on 07.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
